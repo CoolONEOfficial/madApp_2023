c03a43b8a887c58c82c86ef8097998958d6934a3
Nikolai Trukhin <n.trukhin@uzum.com>
2022-12-24 14:58:23 +0100

    Initial Commit

---
+++ madApp/madAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  madAppApp.swift
+//  madApp
+//
+//  Created by Nikolay Truhin on 24/12/22.
+//
+
+import SwiftUI
+
+@main
+struct madAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
